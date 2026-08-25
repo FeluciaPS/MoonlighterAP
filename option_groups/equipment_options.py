8e245cce48438292b16f650aac68e49d3bb7f90c
FeluciaPS <TheMezStrikes@gmail.com>
2026-08-25 13:02:44 +0200

    Add equipment options

---
+++ option_groups/equipment_options.py
@@ -0,0 +1,49 @@
+from Options import Choice, OptionSet, Toggle
+
+
+class EquipmentRandomizer(Choice):
+    """
+    Determines the way armour and weapons are randomised
+    
+    - Fully Random: Shuffles all weapons and armour into the pool randomly
+    - Progressive: Progressively unlocks each equipemnt type, starting with the weakest, then the second, etc.
+    - Tiered: Unlocks entire tiers of equipment at a time, unlocking Tier 1 allows you to craft all Tier 1 Equipment if you have the materials
+    """
+    display_name = "Equipment Randomizer"
+
+    option_fully_random = 0
+    option_progressive = 1
+    option_tiered = 2
+
+    default = 1
+
+class IncludedEquipment(OptionSet):
+    """
+    Choose the equipment that should be included in the randomizer. Other equipment may be included
+    as filler items, but will not be considered in logic.
+
+    If no weapons or no armor is selected, they also will not appear as filler items.
+    """
+    display_name = "Included Equipment"
+
+    valid_keys = [
+        "_allweapons",
+        "_allarmor",
+        "Cloth Armor",
+        "Iron Armor",
+        "Steel Armor",
+        "Short Swords",
+        "Big Swords",
+        "Spears",
+        "Gloves",
+        "Bows"
+    ]
+
+    default = [key for key in valid_keys.copy() if not key.startswith("_")]
+
+class BroomOnly(Toggle):
+    """
+    Removes all weapons from the itempool and shops, making you beat the game with only the
+    broom spear.
+    """
+    display_name = "Broom Only"
